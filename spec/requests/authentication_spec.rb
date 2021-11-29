ec91bf2e80d59dfd148446745caf40d95d195304
Deng <donggdeng@gmail.com>
2021-11-29 16:16:16 +0800

    add password to user

--- spec/requests/authentication_spec.rb
+++ spec/requests/authentication_spec.rb
@@ -2,19 +2,19 @@ require 'rails_helper'
 
 describe 'Authentication', type: :request do
     describe 'POST /authenticate' do
-        let(:user) {FactoryBot.create(:user, username: 'BookSeller99')}
+        let(:user) {FactoryBot.create(:user, username: 'BookSeller99', password: 'Password1')}
 
         it 'should authenticate the client' do
-            post '/api/v1/authenticate', params: { username: user.username, password: 'Password'}
+            post '/api/v1/authenticate', params: { username: user.username, password: 'Password1'}
 
             expect(response).to have_http_status(:created)
             expect(response_body).to eq({
-                'token' => '123'
+                'token' => 'eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxfQ.DiPWrOKsx3sPeVClrm_j07XNdSYHgBa3Qctosdxax3w'
             })
         end
 
         it 'should return error when username is missing' do
-            post '/api/v1/authenticate', params: { password: 'Password'}            
+            post '/api/v1/authenticate', params: { password: 'Password1'}            
 
             expect(response).to have_http_status(:unprocessable_entity)
             expect(response_body.has_key?('error')).to be_truthy
@@ -22,11 +22,17 @@ describe 'Authentication', type: :request do
         end
         
         it 'should return error when password is missing' do
-            post '/api/v1/authenticate', params: { username: 'BookSeller99'}
+            post '/api/v1/authenticate', params: { username: user.username}
 
             expect(response).to have_http_status(:unprocessable_entity)
             expect(response_body.has_key?('error')).to be_truthy
             expect(response_body['error']).to include('param is missing or the value is empty: password')
+        end
+
+        it 'should return error when password is incorrect' do
+            post '/api/v1/authenticate', params: { username: user.username, password: 'incorrect'}
+
+            expect(response).to have_http_status(:unauthorized)
         end
     end
 end
